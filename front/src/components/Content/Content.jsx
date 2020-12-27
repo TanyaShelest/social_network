c67b1aa8d5ab279d862cc94214c3af00b27f9a3c
Tanya Shelest <tashelest@gmail.com>
2020-12-27 22:21:10 +0200

    Describe propTypes for userData object

--- front/src/components/Content/Content.jsx
+++ front/src/components/Content/Content.jsx
@@ -4,22 +4,24 @@ import PostList from './Post/PostList';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 import {pages} from '../../pages';
 import PropTypes from 'prop-types';
+import { userDataType } from './ProfileInfo/ProfileInfoType';
 
-function Content({displayedContent, userName}) {
+function Content({displayedContent, userName, userData}) {
 
   return (
     <div className="content">
 
       { (displayedContent === pages.articles) && <PostList /> } 
       { (displayedContent === pages.addArticle) && <AddPost /> } 
-      { (displayedContent === pages.profileInfo) && <ProfileInfo userName={userName} /> } 
+      { (displayedContent === pages.profileInfo) && <ProfileInfo userName={userName} userData={userData} /> } 
     </div>
   );
 }
  
 Content.propTypes = {
   displayedContent: PropTypes.string.isRequired,
-  userName: PropTypes.string
+  userName: PropTypes.string,
+  userData: userDataType
 };
 
 export default Content;
--- front/src/components/Content/ProfileInfo/ProfileInfo.jsx
+++ front/src/components/Content/ProfileInfo/ProfileInfo.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {userDataType} from './ProfileInfoType';
 
-function ProfileInfo({userName}) {
-    
+function ProfileInfo({userName, userData}) {
+
   return (
     <div>
        <h1>Welcome, {userName || 'anon'}</h1>
+      <ul>
+        <li>{userData.id}</li>
+        <li>{userData.age}</li>
+        <li>{userData.avatar.file.name}</li>
+        <li>{userData.posts[0].title}</li>
+        <li>{userData.posts[0].createdAt}</li>
+      </ul>
     </div>
   );
 }
 
 ProfileInfo.propTypes = {
-  userName: PropTypes.string.isRequired
+  userName: PropTypes.string,
+  userData: userDataType
 };
 
 export default ProfileInfo;
