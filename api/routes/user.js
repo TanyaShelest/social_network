b0061aae26251a5c6e4fbe67d6ea81ee625907e8
Tanya Shelest <tashelest@gmail.com>
2021-01-09 20:51:54 +0200

    Redesign project structure considering DI Container

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,18 +1,19 @@
-module.exports = app => {
+const router = require("express").Router();
 
-    app.get("/user/:id(\\d+)", (req, res) => {
-        res.send("Get a user");
-    });
+router.get("/user/:id(\\d+)", (req, res) => {
+  res.send("Get a user");
+});
 
-    app.get("/user/:id(\\d+)/friends", (req, res) => {
-        res.send("Get user's friends");
-    });
+router.get("/user/:id(\\d+)/friends", (req, res) => {
+  res.send("Get user's friends");
+});
 
-    app.put("/user/:id(\\d+)/update", (req, res) => {
-        res.send("Update user's profile");
-    });
+router.put("/user/:id(\\d+)/update", (req, res) => {
+  res.send("Update user's profile");
+});
 
-    app.delete("/user/:id(\\d+)/delete", (req, res) => {
-        res.send("Delete user's profile");
-    });
-};
+router.delete("/user/:id(\\d+)/delete", (req, res) => {
+  res.send("Delete user's profile");
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const ConfigWrapper = require("./config-wrapper");
 const app = express();
-
+const ConfigWrapper = require("./config-wrapper");
 const config = new ConfigWrapper();
+const diContainer = require("./services/diContainer");
 
-require("./routes/posts")(app);
-require("./routes/user")(app);
+diContainer.register("db", require("./services/db-connection"));
+app.db = diContainer.get("db");
+
+const postRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+app.use("/api", postRoutes);
+app.use("/api", userRoutes);
 
 app.use((err, req, res, next) => {
   console.error("Error:", err.stack);
   res.status(500);
-  res.json({error: "Something went wrong"});
-  });
+  res.json({ error: "Something went wrong" });
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
